a8618a14fe35c96a1704fcab69525eb22032fa85
Mohsin Imran <2.mohsin.ali.2@gmail.com>
2026-09-15 13:00:07 +0500

    Change Some things

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  allowedDevOrigins: ["192.168.18.90"],
+  allowedDevOrigins: ["192.168.56.1"],
 };
 
 export default nextConfig;
